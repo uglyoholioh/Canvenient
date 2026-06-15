135904e423be84ecaeba1e96e27c64fc8b7fa87b
uglyoholioh <87862128+uglyoholioh@users.noreply.github.com>
2026-06-16 01:31:57 +0800

    feat: schedule backend setup and basic dashboard (next 3 events shown - class, event, exam)

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,8 @@ from routes.auth import router as auth_router
 from routes.categories import router as categories_router
 from routes.tasks import router as tasks_router
 from routes.canvas import router as canvas_router
-from routes.schedule import router as schedule_router
+from routes.schedules import router as schedule_router
+from routes.events import router as events_router
 from schema import initialize_schema
 
 
@@ -26,6 +27,7 @@ app.include_router(academic_modules_router)
 app.include_router(tasks_router)
 app.include_router(canvas_router)
 app.include_router(schedule_router)
+app.include_router(events_router)
 
 
 @app.get("/")
